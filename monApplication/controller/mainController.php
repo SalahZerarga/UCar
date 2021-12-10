611666997c4f8b0c253e3f7da448b9252f525576
SlhZer <salahzerarga1@gmail.com>
2021-12-10 17:12:31 +0100

    configuration de projet

---
+++ monApplication/controller/mainController.php
@@ -0,0 +1,26 @@
+<?php
+
+class mainController
+{
+
+	public static function helloWorld($request,$context)
+	{
+		$context->mavariable='hello world';
+		return context::SUCCESS;
+	}
+
+
+
+	public static function index($request,$context){
+		
+		return context::SUCCESS;
+	}
+	public static function superTest($request,$context)
+	{
+		$context->param1=$request["param1"];
+		$context->param2=$request["param2"];
+		return context::SUCCESS;
+	}
+
+
+}
