a3798e3eb28c179d287df9075b0b3c4d7cd68aa6
SlhZer <salahzerarga1@gmail.com>
2021-12-21 21:10:22 +0100

    Ajouter les actions dans le controller

--- monApplication/controller/mainController.php
+++ monApplication/controller/mainController.php
@@ -22,12 +22,37 @@ class mainController
 	}
 	public static function userTest($request,$context)
 	{
-		echo 'shqdghjgkekf';
+		echo "C'est juste pour tester ";
+		//$context->user= utilisateurTable::getUserById($request["user"],$request["pwd"]);
 		$context->user= utilisateurTable::getUserByLoginAndPass($request["user"],$request["pwd"]);
-		echo $context->user;
+		
 		return context::SUCCESS;
 	}
-	
 
+	public static function reservationTest($request,$context)
+	{
+		echo "C'est juste pour tester ";
+		//$context->reservation= reservationTable::getReservationById($request["id"]);
+		$context->reservation= reservationTable::getReservationByVoyage($request["voyage"]);
+		return context::SUCCESS;
+	}
+
+	public static function trajetTest($request,$context)
+	{
+		echo "C'est juste pour tester ";
+		//$context->trajet= trajetTable::getTrajetById($request["id"]);
+		$context->trajet= trajetTable::getTrajet($request["depart"],$request["arrivee"]);
+		return context::SUCCESS;
+	}
+
+	public static function voyageTest($request,$context)
+	{
+		echo "C'est juste pour tester ";
+		//$context->voyage= voyageTable::getVoyageById($request["id"]);
+		$context->voyage= voyageTable::getVoyageByTrajet($request["trajet"]);
+		
+		return context::SUCCESS;
+	}
 
 }
+?>
